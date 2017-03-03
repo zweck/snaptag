26db8ff13137cd0797cdde335134efa6e5b163f6
Phil Hauser <phil@zweck.co>
2017-03-03 09:21:13 +0000

    Initial commit with basic view

---
+++ index.ios.js
@@ -0,0 +1,70 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  NavigatorIOS,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class snaptag extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: initialRoute,
+          title: 'snaptag',
+          rightButtonTitle: 'Select Photo',
+          leftButtonTitle: '#',
+        }}
+        style={{flex: 1}}
+      />
+    );
+  }
+}
+
+class initialRoute extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('snaptag', () => snaptag);
